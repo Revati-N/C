b506916139f11a830d80540ccfac40b3e74c3a9c
Revati Natu <revatinatu@hotmail.com>
2024-06-07 19:03:41 +0530

    Data Types

---
+++ CPP/Basics/datatype.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name = "Revs";
+    int ages = 19;
+    double gpa = 9.2;
+    char grade = 'O';
+    bool isFEMALE = true;
+
+    cout << "My name is " << name <<". My age is " <<ages << ". My CGPA is "<< gpa<<". My grade is "<<grade<<endl;
+
+    return 0;
+}
