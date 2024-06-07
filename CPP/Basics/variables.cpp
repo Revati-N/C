8d3aa672ac8d8953ac4506b5a0390d8b38121afc
Revati Natu <revatinatu@hotmail.com>
2024-06-07 18:52:09 +0530

    Variables Declaration

---
+++ CPP/Basics/variables.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string name = "Revs";
+    int ages = 19;
+    cout << "My Name is "<<name<< endl;
+    cout << "My Age is "<< ages << endl;
+    return 0;
+}
