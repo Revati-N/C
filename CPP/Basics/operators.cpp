6231d0311dce6d8475c98e3147476d4df3e0f87e
Revati Natu <revatinatu@hotmail.com>
2024-06-08 13:53:26 +0530

    Operations in cpp

---
+++ CPP/Basics/operators.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double x,y;
+    cout << "Enter first number: ";
+    cin >> x;
+    cout << "Enter second number: ";
+    cin >> y;
+    cout << "Maths Operations:" << endl;
+    cout << "Addition: " << x+y << endl << "Subtraction: "<< x-y << endl << "Multiplication: "<< x*y << endl << "Division: "<< x/y <<endl;
+}
