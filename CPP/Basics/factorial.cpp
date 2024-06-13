ff6fffb701ec6335a861d018adffcec31cd00083
Revati Natu <revatinatu@hotmail.com>
2024-06-13 23:35:09 +0530

    factorial

---
+++ CPP/Basics/factorial.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int fact(int n)
+{
+    if (n==1 || n == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * fact (n-1);
+    }
+}
+
+int main()
+{
+    int x;
+    cout << "Enter number to factorial: ";
+    cin >> x;
+    cout << fact (x);
+}
