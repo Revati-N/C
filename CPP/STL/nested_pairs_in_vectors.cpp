f68d56bf0b48e858c1e4ee4e022c20e881f4a24e
Revati Natu <revatinatu@hotmail.com>
2024-06-26 23:46:03 +0530

    nesting in vectors

---
+++ CPP/STL/nested_pairs_in_vectors.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void printVec(vector <pair<int,int>> &v){
+    cout << "Size: "<<v.size()<<endl;
+    for(int i =0; i <v.size(); ++i){
+        cout << v[i].first<<" "<< v[i].second<<endl;
+    }
+}
+
+int main() {
+    vector <pair<int,int>>v = {
+        {1,2},{2,3},{3,4},{4,5}};
+    printVec(v);
+    
+    return 0;
+}
